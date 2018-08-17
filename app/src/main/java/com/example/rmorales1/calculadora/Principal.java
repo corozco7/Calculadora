fa72e4b9467220bb7369a22d83dc171be883f2cc
Cesar <c.orozco1293@gmail.com>
2018-08-16 20:35:37 -0500

    Funcionalidad sin validaciones

--- app/src/main/java/com/example/rmorales1/calculadora/Principal.java
+++ app/src/main/java/com/example/rmorales1/calculadora/Principal.java
@@ -1,13 +1,70 @@
 package com.example.rmorales1.calculadora;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+    // Declaro las variables a trabajar
+    private EditText n1, n2;
+    private TextView res;
+    private Resources recursos;
+    private Spinner operaciones;
+    private String op[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        // Capturo los objetos que estan en el layout principal
+        n1 = findViewById(R.id.txtPrimerNumero);
+        n2 = findViewById(R.id.txtSegundoNumero);
+        res = findViewById(R.id.txtResultado);
+        operaciones = findViewById(R.id.cmbOpciones);
+
+        // En una variable guadrdo la referencia a los recursos de la aplicacion
+        recursos = this.getResources();
+        // Capturo el array de las opciones
+        op = recursos.getStringArray(R.array.opciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item,op);
+        operaciones.setAdapter(adapter);
     }
+
+    public void calcular (View v){
+        int opcion;
+        double num1, num2, resultado=0;
+        opcion = operaciones.getSelectedItemPosition();
+        num1 = Double.parseDouble(n1.getText().toString());
+        num2 = Double.parseDouble(n2.getText().toString());
+        switch (opcion){
+            case 1:
+                resultado = Metodos.sumar(num1,num2);
+                break;
+            case 2:
+                resultado = Metodos.restar(num1,num2);
+                break;
+            case 3:
+                resultado = Metodos.multiplicar(num1,num2);
+                break;
+            case 4:
+                resultado = Metodos.dividir(num1,num2);
+                break;
+        }
+        res.setText(String.format("%.2f",resultado));
+    }
+
+    public void limpiar (View v){
+        n1.setText("");
+        n2.setText("");
+        operaciones.setSelection(0);
+        n1.requestFocus();
+    }
+
 }
