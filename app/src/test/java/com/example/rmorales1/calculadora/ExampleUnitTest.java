f737a42480c37c541b572b9bbb226557c935afac
Cesar <c.orozco1293@gmail.com>
2018-08-23 19:50:32 -0500

    Calculadora testeada

--- app/src/test/java/com/example/rmorales1/calculadora/ExampleUnitTest.java
+++ app/src/test/java/com/example/rmorales1/calculadora/ExampleUnitTest.java
@@ -11,7 +11,31 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void sumar_dos_numeros(){
+        double a = 4, b=6, c;
+        c = 10;
+        assertEquals(c,Metodos.sumar(a,b),0);
     }
+
+    @Test
+    public void restar_dos_numeros(){
+        double a = 7, b=6, c;
+        c = 1;
+        assertEquals(c,Metodos.restar(a,b),0);
+    }
+
+    @Test
+    public void multiplicar_dos_numeros(){
+        double a = 7, b=5, c;
+        c = 35;
+        assertEquals(c,Metodos.multiplicar(a,b),0);
+    }
+
+    @Test
+    public void dividir_dos_numeros(){
+        double a = 15, b=3, c;
+        c = 5;
+        assertEquals(c,Metodos.dividir(a,b),0);
+    }
+
 }
